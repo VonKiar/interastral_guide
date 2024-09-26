24b3d79309a82d834136a701203315aa7b0e3019
Potchara J <59822724+VonKiar@users.noreply.github.com>
2024-09-27 06:07:11 +0700

    Add data success

--- data-bank/app.py
+++ data-bank/app.py
@@ -1,21 +1,35 @@
 from flask import Flask
 from flask_restful import Api, Resource
 from flask_pymongo import PyMongo
+from pymongo import MongoClient
+
+uri = "mongodb+srv://interastral_guide01:Z2sFxJB4uhSe1btC@interastralguidetest01.0yhgl.mongodb.net/interastral_guide01?retryWrites=true&w=majority"
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb+srv://interastral_guide01:Z2sFxJB4uhSe1btC@interastralguidetest01.0yhgl.mongodb.net/?retryWrites=true&w=majority&appName=interastralGuideTest01"  # Replace with your MongoDB connection string
+app.config["MONGO_URI"] = uri
 
 mongo = PyMongo(app)
-print(mongo)
 
-@app.route('/')
-def home():
-    return "Welcome to the Interastral Guide API!"
+client = MongoClient(uri)
+try:
+    # Attempt to ping the database
+    client.admin.command('ping')
+    print("MongoDB connection successful!")
+except Exception as e:
+    print("MongoDB connection failed:", e)
+finally:
+    # Close the connection
+    client.close()
+
+db = mongo._database = PyMongo(app).db
 
 class Character(Resource):
     def get(self):
         characters = mongo.db.characters.find()
         return [{"id": str(character["_id"]), "name": character["name"], "power": character["power"]} for character in characters]
+
+def home():
+    return "Welcome to the Interastral Guide API!"
 
 @app.route('/insert_sample_data')
 def insert_sample_data():
@@ -24,8 +38,7 @@ def insert_sample_data():
         {"name": "Character Two", "power": "Water"},
     ]
     try:
-        mongo.db.characters.insert_many(sample_data)
-        print(mongo)
+        db.characters.insert_many(sample_data)
         return "Sample data inserted!", 201  # Returning a 201 Created status
     except Exception as e:
         return str(e), 500  # Return error message with a 500 Internal Server Error status
